52c3d0185e7f69411de3574e5c03c33b643de04e
abeerseada <121635964+abeerseada@users.noreply.github.com>
2024-12-16 17:44:57 +0200

    some updates

--- ShellScript/calculator-2.sh
+++ ShellScript/calculator-2.sh
@@ -5,7 +5,8 @@ echo "1. Add"
 echo "2. Subtract"
 echo "3. Multiply"
 echo "4. Divide"
-echo "5. Quit"
+echo "5. Average"
+echo "6. Quit"
 read -p "Enter ur choice: " choice
 case $choice in 
     1) read -p "Enter Number1: " number1
@@ -24,7 +25,10 @@ case $choice in
         read -p "Enter Number2: " number2
         echo Answer=$(( $number1 / $number2 ))
         ;;
-    5)         break ;;
+    6)         break ;;
+    5) read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(echo " ($number1 + $number2) / 2" | bc -l )    
     *) contiue ;;
 esac
 
